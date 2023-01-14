3e29170a8bcc693b74c9d2aeca76322b9921748e
erickpicos <32298145+erickpicos@users.noreply.github.com>
2023-01-13 23:40:46 -0300

    Complete Test

---
+++ Assets/BossBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossBehaviour : Character
+{
+
+    public int life = 5;
+    
+    public override void DealDamage(float val)
+    {
+        life -= 1;
+        Debug.Log(life);
+        if (life == 0)
+        {
+            GameLogic.instance.EndBoss();
+            ConsoleText.getInstance().ShowMessage("Boss defeat");
+            
+            base.DealDamage(val);
+        }
+        else
+        {
+            GameObject thisExplosionObject = GameObject.Instantiate(explosionFX);
+            thisExplosionObject.transform.position = transform.position;
+            
+        }
+        
+    }
+    
+
+   
+}
